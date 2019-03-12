268d14ea767cf8d505accacd6ee7670100c6b282
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2019-03-12 20:20:41 +0200

    Added documentation for Unauthenticated class

--- app/models/errors/Unauthenticated.scala
+++ app/models/errors/Unauthenticated.scala
@@ -2,7 +2,11 @@ package models.errors
 
 import sangria.execution.UserFacingError
 
-//todo add docs
+/**
+  * Represents an exception object indicating that you are need to be authenticated to perform operation.
+  *
+  * @param msg an exception message to show
+  */
 case class Unauthenticated(msg: String) extends Exception(msg) with UserFacingError {
   override def getMessage(): String = msg
 }
