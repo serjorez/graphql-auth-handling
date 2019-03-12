76249b6c3ae1a42ac0648f0e000cef1bd07ca749
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2019-03-12 18:12:49 +0200

    Added documentation for Tokens entity

--- app/models/jwt/Tokens.scala
+++ app/models/jwt/Tokens.scala
@@ -1,3 +1,9 @@
 package models.jwt
 
+/**
+  * An entity that contains access and refresh token.
+  *
+  * @param accessToken  an access token
+  * @param refreshToken a refresh token
+  */
 case class Tokens(accessToken: String, refreshToken: String)
