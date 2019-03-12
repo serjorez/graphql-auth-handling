461ac5a38f205a8f303e799157c20f13b13d779b
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2019-03-12 18:12:40 +0200

    Added documentation for JwtContent entity

--- app/models/jwt/JwtContent.scala
+++ app/models/jwt/JwtContent.scala
@@ -2,8 +2,13 @@ package models.jwt
 
 import play.api.libs.json._
 
+/**
+  * An entity that is contained in the JWT token.
+  *
+  * @param id user identifier
+  */
 case class JwtContent(id: Long)
 
 object JwtContent {
-  implicit val jwtContentFormat = Json.format[JwtContent]
+  implicit val jwtContentFormat: OFormat[JwtContent] = Json.format[JwtContent]
 }
