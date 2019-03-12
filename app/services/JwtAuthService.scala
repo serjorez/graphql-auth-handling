a6695aa70c544980814483e228cf61cea2eb968c
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2019-03-12 20:14:20 +0200

    Added documentation for JwtAuthService

--- app/services/JwtAuthService.scala
+++ app/services/JwtAuthService.scala
@@ -4,17 +4,62 @@ import models.jwt.{JwtContent, Tokens}
 
 import scala.util.Try
 
+/** Provide the main logic around the manipulation with JWT.
+  *
+  * @define content representation of a content of a token claim
+  * @define token   a JSON Web Token as a Base64 url-safe encoded String which can be used inside an HTTP header
+  * @define secret  an encoded String which can be used in a validation step
+  */
 trait JwtAuthService {
 
+  /** Create access token (JWT with a short lifetime).
+    * Access token's secret should contain only server secret (that should be stored in implementation).
+    *
+    * @return $token
+    * @param content $content
+    */
   def createAccessToken(content: JwtContent): String
 
+  /** Create refresh token (JWT with a long lifetime).
+    * Refresh token's secret should contain server secret + any user-specific information.
+    *
+    * @return $token
+    * @param content $content
+    * @param secret  $secret
+    */
   def createRefreshToken(content: JwtContent, secret: String): String
 
+  /** Create access and refresh tokens.
+    * Access token's secret should contain only server secret (that should be stored in implementation).
+    * Refresh token's secret should contain server secret + any user-specific information.
+    *
+    * @return the access and refresh token
+    * @param content $content
+    * @param secret  $secret
+    */
   def createTokens(content: JwtContent, secret: String): Tokens
 
+  /** Decode token content without signature validation.
+    *
+    * @return $content
+    * @param token $token
+    */
   def decodeContent(token: String): Try[JwtContent]
 
+  /** Decode access token (JWT with a short lifetime).
+    * Access token's secret should contain only server secret (that should be stored in implementation).
+    *
+    * @return $content
+    * @param token $token
+    */
   def decodeAccessToken(token: String): Try[JwtContent]
 
+  /** Decode refresh token (JWT with a long lifetime).
+    * Refresh token's secret should contain server secret + any user-specific information.
+    *
+    * @return $content
+    * @param token  $token
+    * @param secret $secret
+    */
   def decodeRefreshToken(token: String, secret: String): Try[JwtContent]
 }
--- app/services/JwtAuthServiceImpl.scala
+++ app/services/JwtAuthServiceImpl.scala
@@ -12,26 +12,38 @@ import pdi.jwt.exceptions.JwtExpirationException
 
 import scala.util.Try
 
+/**
+  * Default implementation of JwtAuthService, using the HMAC algorithm to encode and decode secret.
+  *
+  * @param jwtConfig JWT configs provider
+  * @param algorithm injected implementation of HMAC algorithm
+  */
 class JwtAuthServiceImpl @Inject()(jwtConfig: JwtConfig,
                                    algorithm: JwtHmacAlgorithm) extends JwtAuthService {
 
+  /** @inheritdoc */
   override def createAccessToken(content: JwtContent): String =
     Jwt.encode(JwtClaim(Json.toJson(content).toString()).issuedNow.expiresIn(jwtConfig.accessTokenExpiration), jwtConfig.secret, algorithm)
 
+  /** @inheritdoc */
   override def createRefreshToken(content: JwtContent, secret: String): String =
     Jwt.encode(JwtClaim(Json.toJson(content).toString()).issuedNow.expiresIn(jwtConfig.refreshTokenExpiration), jwtConfig.secret + secret, algorithm)
 
+  /** @inheritdoc */
   override def createTokens(content: JwtContent, secret: String): Tokens =
     Tokens(createAccessToken(content), createRefreshToken(content, secret))
 
+  /** @inheritdoc */
   override def decodeContent(token: String): Try[JwtContent] = withExceptionTransforming {
     Jwt.decodeRaw(token, JwtOptions(signature = false)).map(Json.parse(_).as[JwtContent])
   }
 
+  /** @inheritdoc */
   override def decodeAccessToken(token: String): Try[JwtContent] = withExceptionTransforming {
     Jwt.decodeRaw(token, jwtConfig.secret, Seq(algorithm)).map(Json.parse(_).as[JwtContent])
   }
 
+  /** @inheritdoc */
   override def decodeRefreshToken(token: String, secret: String): Try[JwtContent] = withExceptionTransforming {
     Jwt.decodeRaw(token, jwtConfig.secret + secret, Seq(algorithm)).map(Json.parse(_).as[JwtContent])
   }
