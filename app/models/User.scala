5826081c85159061fc6b2ab7887dac60a90d8dca
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2019-03-12 18:03:20 +0200

    Added documentation for User entity

--- app/models/User.scala
+++ app/models/User.scala
@@ -3,11 +3,22 @@ package models
 import slick.jdbc.H2Profile.api.{Table => SlickTable, _}
 import slick.lifted.{Tag => SlickTag}
 
+/**
+  * The User entity.
+  *
+  * @param id       an id of the user
+  * @param role     a role of the user
+  * @param login    a login of the user
+  * @param password a password of the post
+  */
 case class User(id: Option[Long] = None,
                 role: String = User.role.USER,
                 login: String,
                 password: String)
 
+/**
+  * Defined slick table for entity 'User'
+  */
 object User extends ((Option[Long], String, String, String) => User) {
 
   class Table(slickTag: SlickTag) extends SlickTable[User](slickTag, "USERS") {
@@ -22,6 +33,9 @@ object User extends ((Option[Long], String, String, String) => User) {
     def * = (id.?, role, login, password).mapTo[User]
   }
 
+  /**
+    * Defined roles that are inherent in the user
+    */
   object role {
     val USER: String = "user"
     val ADMIN: String = "admin"
